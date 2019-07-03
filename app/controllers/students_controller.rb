0a39864bf48c756bc7a7e5cacc296b0d7750ae7a
jmoypace <jmoypace@gmail.com>
2019-07-03 16:43:58 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def actice
+  def active
     @student = Student.find(params[:id])
     #@student.active = !@student.active
     @student.save
